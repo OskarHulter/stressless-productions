152295bcb4ae14e8b1b39b688283fe988bc6b5fc
Oskar Hulter <oskar.hulter@sigma.se>
2019-04-24 15:36:11 +0200

    removed a file.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import classnames from 'classnames'
 import * as css from './Home.css'
 
-export const Home: React.FunctionComponent = props =>
+export const Home: React.FC = props =>
   <div className={classnames('test', css.home)}>
     <ul>
       <li>
