4138c7723abe737ee4688da06abb85235e16ccd9
Oskar Hulter <oskar.hulter@sigma.se>
2019-04-28 21:14:19 +0200

    added global styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,20 @@ const Title = styled.h1`
   color: red;
 `
 
-export const Footer: React.FunctionComponent = props =>
-  <footer>
-    <title>Stressless productions</title>
-  </footer>
+const StyledFooter = styled.footer`
+  color: red;
+  background: purple;
+`
+
+interface FooterProps extends React.Props<FooterProps> {
+  tagline: string
+  contactDetails: string
+  className: string
+  disabled: boolean
+}
+
+
+export const Footer: React.FC<FooterProps> = props =>
+  <StyledFooter>
+    <Title>Stressless productions</Title>
+  </StyledFooter>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,25 +2,46 @@ import * as React from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import Head from 'next/head'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 
+const theme = {
+  colorPrimary: '#7037BF',
+  colorSecondary: '#FF83C6',
+  colorBackground: '#0057AC',
+  colorText: '#E8CCC3',
+  colorAffirmative: '#11819C',
+  colorNegative: '#D92563',
+  colorCaution: '#FEDF00'
+}
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  --colorPrimary: ${props => props.theme.colorPrimary};
+    --colorSecondary: ${props => props.theme.colorSecondary};
+    --colorBackground: ${props => props.theme.colorBackground};
+    --colorText: ${props => props.theme.colorText};
+    --colorAffirmative: ${props => props.theme.colorAffirmative};
+    --colorNegative: ${props => props.theme.colorNegative};
+    --colorCaution: ${props => props.theme.colorCaution};
+} 
+body {
+  color: var(--colorPrimary);
+  background: var(--colorBackground);
+}
+`
 export const Layout: React.FC = props =>
-  <div id="layout">
-    {/*language=PostCSS*/}
-    <style jsx global>{`
-        #layout {
-            background-color: darkolivegreen;
-        }
-    `}
-    </style>
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
-    </Head>
-    <Header />
-    <main>
-      {props.children}
-    </main>
-    <Footer />
-  </div>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <Header />
+      <main>
+        {props.children}
+      </main>
+      <Footer />
+    </GlobalStyle>
+  </ThemeProvider>
