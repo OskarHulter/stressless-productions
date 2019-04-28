950590f3950a3a1268b56b08ffe41fc37196972c
OskarHulter <stresslessness91@gmail.com>
2019-04-28 04:05:24 +0200

    linting

--- src/components/TextAreaInput.tsx
+++ src/components/TextAreaInput.tsx
@@ -1,55 +1,55 @@
 import * as React from 'react'
-import InputError from './InputError';
+import InputError from './InputError'
 
 interface TextAreaInputProps extends React.Props<TextAreaInputProps> {
-    labelText: string,
-    errorMessage: string,
-    value: string,
-    id: string,
-    placeholder: string,
-    className: string,
-    labelClassName: string,
-    disabled: boolean,
-    name: string,
-    handleOnChange: () => any
+	labelText: string
+	errorMessage: string
+	value: string
+	id: string
+	placeholder: string
+	className: string
+	labelClassName: string
+	disabled: boolean
+	name: string
+	handleOnChange: () => any
 }
 
 const TextAreaInput: React.FC<TextAreaInputProps> = props => {
-    let inputClassName = `input${props.className ? ` ${props.className}` : ''}`
-    const forId = props.id || props.name
+	let inputClassName = `input${props.className ? ` ${props.className}` : ''}`
+	const forId = props.id || props.name
 
-    if (props.errorMessage) {
-        inputClassName = `${inputClassName} red-border`
-    }
+	if (props.errorMessage) {
+		inputClassName = `${inputClassName} red-border`
+	}
 
-    if (props.disabled) {
-        inputClassName = `${inputClassName} disabled-input`
-    }
+	if (props.disabled) {
+		inputClassName = `${inputClassName} disabled-input`
+	}
 
-    return (
-        <>
-            <label className={props.labelClassName} htmlFor={forId} >
-                {props.labelText}
-            </label>
-            <textarea
-                // {…props} How to deal with spread i TS?
-                className={inputClassName}
-                id={forId}
-                disabled={props.disabled}
-                onChange={props.handleOnChange}
-            />
-            <InputError errorMessage={props.errorMessage} />
-        </>
-    )
+	return (
+		<>
+			<label className={props.labelClassName} htmlFor={forId}>
+				{props.labelText}
+			</label>
+			<textarea
+				// {…props} How to deal with spread i TS?
+				className={inputClassName}
+				id={forId}
+				disabled={props.disabled}
+				onChange={props.handleOnChange}
+			/>
+			<InputError errorMessage={props.errorMessage} />
+		</>
+	)
 }
 
 TextAreaInput.defaultProps = {
-    labelText: '',
-    errorMessage: '',
-    placeholder: '',
-    className: '',
-    labelClassName: 'large-heading',
-    disabled: false
+	labelText: '',
+	errorMessage: '',
+	placeholder: '',
+	className: '',
+	labelClassName: 'large-heading',
+	disabled: false
 }
 
 export default TextAreaInput
